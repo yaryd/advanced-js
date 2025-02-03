a6ada74647f3e20b6634be5c2e77251435ee0ec7
efoster9 <98239176+efoster9@users.noreply.github.com>
2025-02-03 15:19:45 +0000

    changes

--- LAB1/practice/script.js
+++ LAB1/practice/script.js
@@ -1,4 +1,4 @@
-// Fundamental data types
+// List 4 data types in a multiline comment
 
 // Typeof
 
@@ -6,30 +6,24 @@
 
 // Reassignment (no let)
 
-
 // Essential Operations
 
 // Additional operations
 
 // User Input
 
-
 // Concatenation v. templates
 
-// String methods
+// String Methods (uppercase, lowercase, includes)
 
+// Math methods
 
 // Comparison operators - true or false?
 
-
 // Conditional statements;
-
-
 
 // Functions
 
 // Returns half the number if it can be halved; otherwise returns false
 
-
 // Functions can also be written as arrows. Body of function doesn't change, just the signature.
-
